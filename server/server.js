2a1339cd45b6f73294fee0761d06c8c876dafe1a
KSHITIZJAIN00 <114683009+KSHITIZJAIN00@users.noreply.github.com>
2025-04-12 19:34:26 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
 const weatherRoute = require("./routes/weather");
+
+dotenv.config(); 
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
+
+
 app.use("/api", weatherRoute);
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
